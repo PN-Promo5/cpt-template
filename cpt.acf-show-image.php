74dd65802e3373ab5c76ba0b4abaff744266f2a6
Alexandra Dubost <alexandra.dubost@gmail.com>
2019-02-15 16:34:40 +0100

    Add comments / Remove unused div

--- cpt.acf-show-image.php
+++ cpt.acf-show-image.php
@@ -1,11 +1,10 @@
 <?php
-// On déclare une variable image qui récupère tout le champ dans un array
-$image = get_field('image_principale');
-// On impose une condition, au cas où il n'y ait pas d'image
-if (!empty($image)):
+	// On déclare une variable image qui récupère tout le champ dans un array
+	// WARNING : on met dans 'the_field()' le nom du champ dans ACF
+	$image = get_field('image_principale');
+	// On impose une condition, au cas où il n'y ait pas d'image
+	if (!empty($image)):
 ?>
-	<div class="recipe-img">
 		<!-- On appelle dans le tableau $image, la clé 'url' et 'alt' -->
 		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-	</div>
 <?php endif; ?>
